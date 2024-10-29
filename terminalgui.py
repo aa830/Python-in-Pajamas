52c01e4bfcfc3eee8708c363fb969b55e14db024
aa830 <84448225+aa830@users.noreply.github.com>
2024-10-29 17:53:23 +0800

    wrong variable....

--- terminalgui.py
+++ terminalgui.py
@@ -144,7 +144,7 @@ def main1(stdscr, course_number):
             (
                 f"Page 1:\n\nWelcome to Course 3, {name}!\n"
                 "In this course, we'll explore how you can start programming!\n\n"
-                f"First, we need to download Python. You can press 'o' to open the Python website in your browser.\n\n{a3}"
+                f"First, we need to download Python. You can press 'o' to open the Python website in your browser.\n\n{a4}"
                 "After downloading, just double-click the file!\n\n"
                 "If there is an option called 'add to PATH', make sure to select it. "
                 "Then all you need to do is click install, and voila! You have Python!\n\n"
